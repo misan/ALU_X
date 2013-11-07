35244ce3418b0caeda87a9f7b78dea8e2bb3fd85
ni9htfury <seadragon76@gmail.com>
2013-11-07 18:07:07 +0800

    change date time variable

--- Configuration.h
+++ Configuration.h
@@ -15,7 +15,7 @@
 // User-specified version info of this build to display in [Pronterface, etc] terminal window during
 // startup. Implementation of an idea by Prof Braino to inform user that any changes made to this
 // build by the user have been successfully uploaded into firmware.
-#define STRING_VERSION_CONFIG_H __01OCT2013__ " " __2200hr__ // build date and time
+#define STRING_VERSION_CONFIG_H 01OCT2013 " " 2200hr // build date and time
 #define STRING_CONFIG_H_AUTHOR "(MEL, ALU-X config)" // Who made the changes.
 
 // SERIAL_PORT selects which serial port should be used for communication with the host.
